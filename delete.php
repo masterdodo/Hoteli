45dd0419a4d62ffaaaee4c36d147075fe3854b9e
masterdodo <dodo.arastava@gmail.com>
2018-09-09 21:53:14 +0200

    Refresh

--- delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once './povezavazbazo.php';
-    
-    $sql = "DELETE FROM hoteli WHERE id_h = " . $_GET['id'];
-    $result = mysqli_query($conn, $sql);
-    
-    header("location: adminpage.php");
-?>
--- preverjanjeadmin.php
+++
@@ -1,6 +0,0 @@
-<?php
-//if (!isset ($_SESSION['uporabnisko_ime']))
-//{
-  //  header("location: prijava.php");
-//}
-?>
