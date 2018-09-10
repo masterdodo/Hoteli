ca722ee3a7fd683e6a5f2e6c7d6c353fad01cc3e
masterdodo <dodo.arastava@gmail.com>
2018-09-10 16:09:57 +0200

    Merge conflicts

---
+++ prijava/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Prijava</title>
+    </head>
+    <body>
+        <form action="checklogin.php" method="POST">
+            <input type="text" name="email" placeholder="E-pošta"><br />
+            <input type="password" name="password" placeholder="Geslo"><br />
+            <input type="submit" name="submit" value="Prijava">
+        </form>
+    </body>
+</html>
---
+++ registracija/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Registracija</title>
+    </head>
+    <body>
+        <form action="add_user.php" method="POST">
+            <input type="text" name="email" placeholder="Epošta"><br />
+            <input type="text" name="username" placeholder="Uporabniško ime"><br />
+            <input type="password" name="password" placeholder="Geslo"><br />
+        </form>
+    </body>
+</html>
