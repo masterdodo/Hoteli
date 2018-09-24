7cbd78a0bb9ecb5e229a06f41ebfbc832816c286
masterdodo <dodo.arastava@gmail.com>
2018-09-24 20:51:10 +0200

    Fix

--- prijava/index.php
+++ prijava/index.php
@@ -8,7 +8,6 @@ else if (isset ($_SESSION['username']))
 {
     header ('location:../');
 }
-rename ('../assets/avatars/default/picture.png', '../assets/avatars/default/pic.png');
 ?>
 <!DOCTYPE html>
 <html>
