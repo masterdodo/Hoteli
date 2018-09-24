4abb0e2f413b4e8e6345a40d619dcc551afafdd2
root <root@aristovnik.com>
2018-09-24 15:49:23 +0200

    Merge/Hosting

--- x/dbconn.php
+++ x/dbconn.php
@@ -1,8 +1,8 @@
 <?php
-$host = '127.0.0.1';
+$host = 'localhost';
 $db   = 'hoteli';
-$user = 'root';
-$pass = '';
+$user = 'phpmyadmin';
+$pass = '1W2E3Radmin';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
