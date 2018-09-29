be71a83a13c1ca2c5494d88e6d4449480c999c2a
unknown <dodo.arastava@gmail.com>
2018-09-29 23:11:50 +0200

    JS auto-alerts prijava css

---
+++ js/prijava.js
@@ -0,0 +1,43 @@
+function checkInputOnKeyUp (x)
+{
+    if( /(.+)@(.+){2,}\.(.+){2,}/.test(x) )
+    {
+        document.querySelector('#error-email').innerHTML = "";
+        document.querySelector('#input-prijava-email').style.background = '#6faf48';
+        document.querySelector('#error-email').style.display = "none";
+    } 
+    else
+    {
+        if (document.querySelector('#error-email').innerHTML == "")
+        {
+            setTimeout(function()
+            {
+                document.querySelector('#error-email').style.display = "block";
+                document.querySelector('#error-email').innerHTML = "Napačna e-pošta."; 
+                document.querySelector('#input-prijava-email').style.background = '#ad2424';
+            }, 500);
+        }
+    }
+}
+
+function checkInputOnKeyUpPass (x)
+{
+    if (x.length < 8)
+    {
+        if (document.querySelector('#error-pass').innerHTML == "")
+        {
+            setTimeout(function()
+            {
+                document.querySelector('#error-pass').style.display = "block";
+                document.querySelector('#error-pass').innerHTML = "Geslo je prekratko.";
+                document.querySelector('#input-prijava-pass').style.background = '#ad2424';
+            }, 500);
+        }
+    }
+    else
+    {
+        document.querySelector('#error-pass').innerHTML = "";
+        document.querySelector('#input-prijava-pass').style.background = '#6faf48';
+        document.querySelector('#error-pass').style.display = "none";
+    }
+}
--- prijava/index.php
+++ prijava/index.php
@@ -16,7 +16,7 @@ else if (isset ($_SESSION['username']))
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
         <title>Prijava</title>
-        <link rel="stylesheet" href="../css/main.css">
+        <link rel="stylesheet" href="../css/prijava.css">
         <script src="../js/main.js"></script>
     </head>
     <body>
