4e43b238e0c4fd48fc4ec2c8c54ede933870666e
unknown <dodo.arastava@gmail.com>
2018-09-29 22:58:22 +0200

    JS auto-alerts prijava css

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ function checkInputOnKeyUp (x)
 {
     if( /(.+)@(.+){2,}\.(.+){2,}/.test(x) )
     {
+        document.querySelector('#error-email').style.display = none;
         document.querySelector('#error-email').innerHTML = "";
-        document.querySelector('#input-prijava-pass').style.display = none;
         document.querySelector('#input-prijava-email').style.background = '#6faf48';
     } 
     else
@@ -12,8 +12,8 @@ function checkInputOnKeyUp (x)
         {
         setTimeout(function()
         {
+            document.querySelector('#error-email').style.display = block;
             document.querySelector('#error-email').innerHTML = "Napačna e-pošta."; 
-            document.querySelector('#input-prijava-pass').style.display = block;
             document.querySelector('#input-prijava-email').style.background = '#ad2424';
         }, 500);
         }
@@ -28,16 +28,16 @@ function checkInputOnKeyUpPass (x)
         {
         setTimeout(function()
         {
+            document.querySelector('#error-pass').style.display = block;
         document.querySelector('#error-pass').innerHTML = "Geslo je prekratko.";
-        document.querySelector('#input-prijava-pass').style.display = block;
         document.querySelector('#input-prijava-pass').style.background = '#ad2424';
         }, 500);
         }
     }
     else
     {
+        document.querySelector('#error-pass').style.display = none;
         document.querySelector('#error-pass').innerHTML = "";
-        document.querySelector('#input-prijava-pass').style.display = none;
         document.querySelector('#input-prijava-pass').style.background = '#6faf48';
     }
 }
