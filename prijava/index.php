0ed5f2a15f807c3a4a078570eb50ccffc502f459
unknown <dodo.arastava@gmail.com>
2018-09-29 21:10:50 +0200

    JS check

--- prijava/index.php
+++ prijava/index.php
@@ -16,6 +16,7 @@ else if (isset ($_SESSION['username']))
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Prijava</title>
         <link rel="stylesheet" href="../css/main.css">
+        <script src="../js/main.js"></script>
     </head>
     <body>
         <div id="login-wrapper">
