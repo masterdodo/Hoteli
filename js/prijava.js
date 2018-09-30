040b3bf0050596e9642d337ef7df9c182fdfb2ed
unknown <dodo.arastava@gmail.com>
2018-09-30 11:52:25 +0200

    JS prijava auto-alerts fix

--- js/prijava.js
+++ js/prijava.js
@@ -1,3 +1,5 @@
+var emailWrong = 0;
+var passWrong = 0;
 function checkInputOnKeyUpMail (x)
 {
     if( /(.+)@(.+){2,}\.(.+){2,}/.test(x) )
@@ -5,7 +7,11 @@ function checkInputOnKeyUpMail (x)
         document.querySelector('#error-email').innerHTML = "";
         document.querySelector('#input-prijava-email').style.background = '#6faf48';
         document.querySelector('#error-email').style.display = "none";
-        document.querySelector('#input-prijava-submit').disabled = false;
+        emailWrong = 0;
+        if (emailWrong == 0 && passWrong == 0)
+        {
+            document.querySelector('#input-prijava-submit').disabled = false;
+        }
     } 
     else
     {
@@ -17,6 +23,7 @@ function checkInputOnKeyUpMail (x)
                 document.querySelector('#error-email').innerHTML = "Napačna e-pošta."; 
                 document.querySelector('#input-prijava-email').style.background = '#ad2424';
                 document.querySelector('#input-prijava-submit').disabled = true;
+                emailWrong = 1;
             }, 500);
         }
     }
@@ -34,6 +41,7 @@ function checkInputOnKeyUpPass (x)
                 document.querySelector('#error-pass').innerHTML = "Geslo je prekratko.";
                 document.querySelector('#input-prijava-pass').style.background = '#ad2424';
                 document.querySelector('#input-prijava-submit').disabled = true;
+                passWrong = 1;
             }, 500);
         }
     }
@@ -42,6 +50,10 @@ function checkInputOnKeyUpPass (x)
         document.querySelector('#error-pass').innerHTML = "";
         document.querySelector('#input-prijava-pass').style.background = '#6faf48';
         document.querySelector('#error-pass').style.display = "none";
-        document.querySelector('#input-prijava-submit').disabled = false;
+        passWrong = 0;
+        if (passWrong == 0 && emailWrong == 0)
+        {
+            document.querySelector('#input-prijava-submit').disabled = false;
+        }
     }
 }
