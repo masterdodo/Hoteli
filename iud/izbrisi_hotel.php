38f1685c765395267de71e37826c9d0f7c2d27cf
masterdodo <dodo.arastava@gmail.com>
2018-10-01 21:35:56 +0200

    delete hotel - delete logins

--- iud/izbrisi_hotel.php
+++ iud/izbrisi_hotel.php
@@ -1,5 +1,7 @@
 <?php
 include ('../x/dbconn.php');
+$sql = 'DELETE FROM hotel_logins WHERE hotel_id = ?';
+$stmt = $pdo->prepare ($sql)->execute ([$_GET['y']]);
 $sql = 'DELETE FROM hotels WHERE id = ?';
 $stmt = $pdo->prepare ($sql)->execute ([$_GET['y']]);
 header ('location:../');
